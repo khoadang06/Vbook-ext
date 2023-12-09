035c497c3065ea3e077cbf57b2e5cd1a2706c2bd
Nahona <134230871+SoulGodEve9x9@users.noreply.github.com>
2023-12-09 20:51:06 +0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,1 +1,20 @@
-
+`{
+  "metadata": {
+    "author": "Nahona",
+    "description": "Nahona"
+  },
+  "data": [
+    {
+      "name": "ixunshu",
+      "author": "vBook",
+      "path": "https://github.com/SoulGodEve9x9/Vbook-ext/blob/d31df3ccb7e55aededc1491da34d1e34db42d8b6/B%C3%BAt%20Th%E1%BB%A7%20C%C3%A1c.zip",
+      "version": 1,
+      "source": "https://ixunshu.net",
+      "icon": "https://raw.githubusercontent.com/Moleys/vbook-ext/main/biqugse/icon.png",
+      "description": "Đọc truyện trên trang ixunshu.net",
+      "type": "chinese_novel",
+      "locale": "zh_CN"
+    }
+  ]
+}
+`
