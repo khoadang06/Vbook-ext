d62a2cc62dcc3b5874305e7c4a0cd80fa8079065
Nahona <134230871+SoulGodEve9x9@users.noreply.github.com>
2023-12-09 20:52:24 +0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -5,11 +5,11 @@
   },
   "data": [
     {
-      "name": "ixunshu",
-      "author": "vBook",
+      "name": "Bút Thủ Các",
+      "author": "Nahona",
       "path": "https://github.com/SoulGodEve9x9/Vbook-ext/blob/d31df3ccb7e55aededc1491da34d1e34db42d8b6/B%C3%BAt%20Th%E1%BB%A7%20C%C3%A1c.zip",
       "version": 1,
-      "source": "https://ixunshu.net",
+      "source": "https://www.bqg9527.net/",
       "icon": "https://raw.githubusercontent.com/Moleys/vbook-ext/main/biqugse/icon.png",
       "description": "Đọc truyện trên trang ixunshu.net",
       "type": "chinese_novel",
