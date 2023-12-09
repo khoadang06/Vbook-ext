654f0c510af3e0de36e005af5c30005363293c2a
Nahona <134230871+SoulGodEve9x9@users.noreply.github.com>
2023-12-09 20:55:07 +0700

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -11,7 +11,7 @@
       "version": 1,
       "source": "https://www.bqg9527.net/",
       "icon": "https://raw.githubusercontent.com/Moleys/vbook-ext/main/biqugse/icon.png",
-      "description": "Đọc truyện trên trang ixunshu.net",
+      "description": "Đọc truyện trên trang bqg9527",
       "type": "chinese_novel",
       "locale": "zh_CN"
     }
